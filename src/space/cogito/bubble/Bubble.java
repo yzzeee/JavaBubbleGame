6190630522b5b5ec661172ecfb87c30f4ad7234d
yzzeee <goooooodgood@gmail.com>
2022-02-13 00:30:46 +0900

    Shoot bubble!

--- src/space/cogito/bubble/Bubble.java
+++ src/space/cogito/bubble/Bubble.java
@@ -31,6 +31,7 @@ public class Bubble extends JLabel implements Moveable {
         this.player = player;
         initObject();
         initSetting();
+        initThread();
     }
 
     private void initObject() {
@@ -53,18 +54,62 @@ public class Bubble extends JLabel implements Moveable {
         state = 0;
     }
 
+    private void initThread() {
+        new Thread(() -> {
+            if (player.getPlayerWay() == PlayerWay.LEFT) {
+                left();
+            } else {
+                right();
+            }
+        }).start();
+    }
+
     @Override
     public void left() {
+        left = true;
+        for (int i = 0; i < 400; i++) {
+            x--;
+            setLocation(x, y);
 
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        up();
     }
 
     @Override
     public void right() {
+        right = true;
+        for (int i = 0; i < 400; i++) {
+            x++;
+            setLocation(x, y);
 
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        up();
     }
 
     @Override
     public void up() {
+        up = true;
+        while (up) {
+            for (int i = 0; i < 400; i++) {
+                y--;
+                setLocation(x, y);
 
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
