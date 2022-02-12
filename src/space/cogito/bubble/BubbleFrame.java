674a66d9cdc4810892c8cec7c4137ca6a339404f
yzzeee <goooooodgood@gmail.com>
2022-02-12 23:17:51 +0900

    Add background image

--- src/space/cogito/bubble/BubbleFrame.java
+++ src/space/cogito/bubble/BubbleFrame.java
@@ -1,30 +1,34 @@
 package space.cogito.bubble;
 
-import javax.swing.JButton;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 // 1. 윈도우 창이 되었음
 // 2. 윈도우 창은 내부에 패널을 하나 가지고 있다.
 public class BubbleFrame extends JFrame {
 
-    private JTextField textField;
+    private JLabel backgroundMap;
 
     public BubbleFrame() {
-        setSize(1000, 640);
-        getContentPane().setLayout(null);
-
-        JButton newBtn = new JButton("New Button");
-        newBtn.setBounds(96, 96, 85, 21);
-        getContentPane().add(newBtn);
-
-        textField = new JTextField();
-        textField.setText("11111");
-        textField.setBounds(376, 251, 96, 19);
-        getContentPane().add(textField);
-        textField.setColumns(10);
-
+        initObject();
+        initSetting();
         setVisible(true); // 그림을 그려라
+    }
+
+    private void initObject() {
+        backgroundMap = new JLabel(new ImageIcon("image/backgroundMap.png"));
+        setContentPane(backgroundMap); // JPanel을 JLabel로 대체함
+//        backgroundMap.setSize(1000, 600);
+//        backgroundMap.setLocation(300, 300);
+//        add(backgroundMap); // JFrame에 JLabel이 그려진다.
+    }
+
+    private void initSetting() {
+        setSize(1000, 640);
+        setLayout(null); // absolute 레이아웃 (자유롭게 그림을 그릴 수 있다.)
+        setLocationRelativeTo(null); // JFrame 가운데 배치하기
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // X 버튼으로 창 끌 때 JVM 같이 종료하기
     }
 
     public static void main(String[] args) {
